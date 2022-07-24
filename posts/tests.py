dff1f373415a080ff5b727aa4a6b3d2797911fd9
Geanderson Esteves <14096071+gesteves91@users.noreply.github.com>
2022-07-23 23:10:38 -0400

    update the tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="just a test")
+
+    def test_model_content(self):
+        post_text = self.post.text
+        self.assertEqual(post_text, "just a test")
+        
+    def test_url_exists_at_correct_location(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_homepage(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
+        self.assertContains(resp, 'just a test')
